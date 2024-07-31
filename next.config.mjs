8b54ef23e9d604c0dd4fb3ff03651b33dc70f137
jangkyum <codedot28@gmail.com>
2024-07-31 17:37:50 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/products/:id', //리다이렉트를 할 주소
+        destination: '/items/:id', // 이동시킬 주소
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/codeitmall/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+import '@/styles/global.css';
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+;
+
+  return(
+    <>    
+    <Head>
+    <title>Head test</title>
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <ThemeProvider>
+    <Header />
+    <Container>
+    <Component {...pageProps} />
+    </Container>
+    </ThemeProvider>
+    </>
+
+  ) 
+  
+
 }
